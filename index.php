7eb036d574caf5ea58b911f8fd771639cf8ccdc3
stephbak <stephane.bakum@sfr.fr>
2018-12-14 16:04:58 +0100

    exercice6

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8" />
+  <title>php partie1</title>
+</head>
+<body>
+  <p>
+    <?php
+    $name = 'Stéphane';
+    echo 'Bonjour ' . $name . ', comment vas tu?';
+    ?>
+  </p>
+</body>
+</html>
